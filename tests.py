378b6bffdee96ffae3418460b93b6e8320574540
TownesZhou <jinchenz@usc.edu>
2022-08-08 19:13:07 -0400

    Full process tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import random
 
 from site_navigator import SiteNavigator
 
@@ -51,8 +52,104 @@ async def first_to_end():
     await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_5_default.png"))
 
 
+async def random_to_end():
+    # Use random waifu in the grid till end
+    navi = await SiteNavigator.create_navi()
+    # stage 1
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_1_grid.png"))
+    # stage 2
+    await navi.continue_grid(random.randrange(0, 15))
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_2_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_2_default.png"))
+    # stage 3
+    await navi.continue_grid(random.randrange(0, 15))
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_3_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_3_default.png"))
+    # stage 4
+    await navi.continue_grid(random.randrange(0, 15))
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_4_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_4_default.png"))
+    # stage 5
+    await navi.continue_grid(random.randrange(0, 15))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_5_default.png"))
+
+
+async def refresh_to_end():
+    # Refresh the grid at each stage, then use first selection of the grid to proceed till end
+    navi = await SiteNavigator.create_navi()
+    # stage 1
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_1_grid.png"))
+    await navi.refresh()
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_1_grid_refresh.png"))
+    # stage 2
+    await navi.continue_grid(0)
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_2_grid.png"))
+    await navi.refresh()
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_2_grid_refresh.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_2_default.png"))
+    # stage 3
+    await navi.continue_grid(0)
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_3_grid.png"))
+    await navi.refresh()
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_3_grid_refresh.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_3_default.png"))
+    # stage 4
+    await navi.continue_grid(0)
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_4_grid.png"))
+    await navi.refresh()
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "stage_4_grid_refresh.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_4_default.png"))
+    # stage 5
+    await navi.continue_grid(0)
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "stage_5_grid.png"))
+
+
+async def first_to_end_and_back():
+    # Use first alternative waifu in the grid till end,
+    # then go back to stage 1
+    navi = await SiteNavigator.create_navi()
+    # stage 1
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "1_grid.png"))
+    # stage 2
+    await navi.continue_grid(0)
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "2_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "2_default.png"))
+    # stage 3
+    await navi.continue_grid(0)
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "3_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "3_default.png"))
+    # stage 4
+    await navi.continue_grid(0)
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "4_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "4_default.png"))
+    # stage 5
+    await navi.continue_grid(0)
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "5_default.png"))
+    # stage 6 (4)
+    await navi.back()
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "6_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "6_default.png"))
+    # stage 7 (3)
+    await navi.back()
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "7_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "7_default.png"))
+    # stage 8 (2)
+    await navi.back()
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "8_grid.png"))
+    await navi.waifu_default_screenshot(os.path.join(test_screenshot_dir, "8_default.png"))
+    # stage 9 (1)
+    await navi.back()
+    await navi.waifu_grid_screenshot(os.path.join(test_screenshot_dir, "9_grid.png"))
+
+
 if __name__ == '__main__':
     if not os.path.exists(test_screenshot_dir):
         os.mkdir(test_screenshot_dir)
+    # Clear all .png files under the test_screenshot_dir
+    for filename in os.listdir(test_screenshot_dir):
+        if filename.endswith(".png"):
+            os.remove(os.path.join(test_screenshot_dir, filename))
 
-    asyncio.get_event_loop().run_until_complete(first_to_end())
+    # Select one of the above functions to test
+    test_func = first_to_end_and_back
+    asyncio.get_event_loop().run_until_complete(test_func())
